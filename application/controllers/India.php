90090d6e20d7b4f61112cb33d30188151c4f7beb
Sergio "Josh" Villegas <joshviba_7@hotmail.com>
2018-02-01 17:21:35 -0800

    Added India controller which returns an image. closes #10

---
+++ application/controllers/India.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of India
+ *
+ * @author Sergio
+ */
+class India extends CI_Controller{
+    function __construct()
+    {
+        parent::__construct();
+    }
+        
+    public function index() {
+        // The following should be a relative link to your image file above
+        $source = '../data/baboon.bmp'; 
+        // note that we could have referenced an image anywhere on our system
+
+        // set the mime type for that image (jpeg, png, etc)
+        header("Content-type: image/bmp"); 
+        header('Content-Disposition: inline');
+        readfile($source); // dish it
+    }
+}
